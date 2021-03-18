23eac5f04c34a0be1305dc4077374a80dade9a91
imakria <imakria@gmail.com>
2021-03-18 10:15:08 +0100

    added firebase ans signup with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { auth } from '../../firebase/firebase.utils';
+
 import { ReactComponent as Logo } from '../../assests/crown.svg';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
+
     <div className='header'>
         <Link className='logo-container' to='/'>
+            {console.log(currentUser)};
             <Logo className='logo' />
         </Link>
         <div className='options'>
@@ -17,6 +21,15 @@ const Header = () => (
             <Link className='option' to='/shop'>
                 CONTACT
             </Link>
+            {currentUser ? (
+                <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+            ) : (
+                <Link className='option' to='/signin'>
+                    SIGN IN
+                </Link>
+            )}
         </div>
     </div>
 );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,56 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDlFnThNew5lZNbnyOq1PpKMSGdSOA-_tI",
+    authDomain: "crwn-db-14772.firebaseapp.com",
+    projectId: "crwn-db-14772",
+    storageBucket: "crwn-db-14772.appspot.com",
+    messagingSenderId: "922367340046",
+    appId: "1:922367340046:web:bdd42728dc226afc68d65f",
+    measurementId: "G-WB1R62BZ9G"
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
+
+
+
+
+
+
